4561c44a4799f26e20b29a4b3fd5f32cfb74be89
Anna-Maria <40987@ma-web.nl>
2025-12-02 14:38:53 +0100

    yes

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Skill_Github_Unity
+
+## opdracht in Unity
+een cube en Debug.Log
+## demo
+![Gif](Les3.gif)
